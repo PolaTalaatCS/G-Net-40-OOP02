23f8a3c41407ab90510a47569bbba8b4b46d20cf
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-22 16:09:56 +0200

    Question 2 (therotical)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace G_Net_40_OOP02
+﻿using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace G_Net_40_OOP02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-#
-        }
+            #region Question 1 (therotical)
+            //a) anyone can change the data directly and there is no validation or control over how the data is modified
+            //b) make attributes private and use properties(get/set) to control access and add validation
+            //c) because they break encapsulation so any code can change the data freely that make the program unsafe and harder to control and harder to maintain
+            #endregion
+            #region Question 2 (therotical)
+            // field  direct data storage and no validation but property controlled access and can validate , yes can contain logic
+            // Example
+            //class rectangle {
+            //private double width;
+          //private double height;
+         //public Rectangle(double w, double h)
+        //    {
+    //        width = w;
+    //        height = h;
+    //        }
+   // public double Area
+    // {
+    // get { return width * height; }  
+    //
+    //}
+    #endregion
+}
     }
 }
