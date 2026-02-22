4fea03c242fd6c6af86ebb730a92fc2bf401dd2e
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-22 17:12:56 +0200

    part 2 question 1

---
+++ Class2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_OOP02
+{
+    #region part 2 question 1
+    internal class Movieticket
+    {
+        private string moviename;
+        private double price;
+
+        public string Moviename
+        {
+            get { return moviename; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    moviename = value;
+            }
+        }
+
+        public TicketType Type { get; set; }
+        public Seat Seat { get; set; }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                    price = value;
+            }
+        }
+        public double PriceAfterTax
+        {
+            get { return price + (price * 14 / 100); }
+        }
+    }
+    #endregion
+}
